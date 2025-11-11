0004137e81b868ac0aa25ca230181aa6bd40931b
Nasty Balandina <askabalandina5556@gmail.com>
2025-11-11 15:18:18 +0300

    homework 1

--- lesson_1/lesson_1_1.py
+++ lesson_1/lesson_1_1.py
@@ -1,1 +1,64 @@
-print("Hello,world!")
+# name = "Nasty"
+# age = 34
+# height = 164.2
+# print(f"имя : {name}, возраст: {age}, рост: {height}")
+
+# x = 10
+# print(type(x))
+# x = 25.5
+# print(type(x))
+# x = "Python"
+# print(type(x),(x))
+
+# a = 7
+# b = 7
+# print(a,b)
+# a = 10
+# b = 7
+# print(b)
+
+# x = y = z = 100
+# print(x,y,z)
+# print(id(x),id(y),id(z))
+# x = y = z = 101, 102, 103
+# print(x,y,z)
+# print(id(x),id(y),id(z))
+
+# a = 1
+# b = 2
+# a, b = b, a
+# print(a,b)
+
+# True = 4
+# print = 3
+# if =9
+# print(True, print, if)
+# import keyword
+# print(keyword.kwlist)
+
+# var1 = 42
+# var2 = 3.14
+# var3 = "Hello"
+# print(type(var1))
+# print(type(var2))
+# print(type(var3))
+# var1 = False
+# print(type(var1))
+
+number_of_students = 25
+GPA = 4.75
+course_name = "Программирование на Python"
+current_rate = True
+items = ["Математика", "Информатика", "Физика"]
+print(type(number_of_students))
+# print(type(GPA))
+# print(type(course_name))
+# print(type(current_rate))
+# print(type(items))
+# переменная = 10
+# print(переменная)
+print(f"количество_студентов = {number_of_students}, тип: {type(number_of_students)}")
+print(f"средний_балл = {GPA}, тип: {type(GPA)}")
+print(f"название_курса = {course_name}, тип: {type(course_name)}")
+print(f"активный_курс = {current_rate}, тип: {type(current_rate)}")
+print(f"список_предметов = {items}, тип: {type(items)}")
