349f8b47f022cb453b0cc4f40465d172a1970a96
Nasty Balandina <askabalandina5556@gmail.com>
2026-01-13 15:12:18 +0300

    homework 13

--- lesson_13/homework_13_1.py
+++ lesson_13/homework_13_1.py
@@ -1,53 +1,53 @@
 1.
-# def uppercase_decorator(func):
-#     def wrapper(*args, **kwargs):
-#         result = func(*args, **kwargs)
-#         return result.upper()
-#     return wrapper
-# @uppercase_decorator
-# def say_hello():
-#     return "hello, world!"
-#
-# print(say_hello())
+def uppercase_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result.upper()
+    return wrapper
+@uppercase_decorator
+def say_hello():
+    return "hello, world!"
+
+print(say_hello())
 
 2.
-# def repeat(n):
-#     def decorator(func):
-#         def wrapper(*args, **kwargs):
-#             for x in range(n):
-#                 func(*args, **kwargs)
-#         return wrapper
-#     return decorator
-#
-#
-# @repeat(2)
-# def hello():
-#     print("Hello!")
-#
-# hello()
+def repeat(n):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for x in range(n):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@repeat(2)
+def hello():
+    print("Hello!")
+
+hello()
 
 
 3.
-# def cache(func):
-#     memory = {}
-#
-#     def wrapper(*args):
-#         if args not in memory:
-#             result = func(*args)
-#             memory[args] = result
-#         else:
-#             result = memory[args]
-#         return result
-#
-#     return wrapper
-#
-# @cache
-# def slow_add(a, b):
-#     print(f"Вычисляю {a} + {b}...")
-#     return a + b
-#
-# print(slow_add(2, 3))
-# print(slow_add(2, 3))
+def cache(func):
+    memory = {}
+
+    def wrapper(*args):
+        if args not in memory:
+            result = func(*args)
+            memory[args] = result
+        else:
+            result = memory[args]
+        return result
+
+    return wrapper
+
+@cache
+def slow_add(a, b):
+    print(f"Вычисляю {a} + {b}...")
+    return a + b
+
+print(slow_add(2, 3))
+print(slow_add(2, 3))
 
 
 4.
