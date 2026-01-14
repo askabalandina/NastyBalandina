b733cba624e69a10329ba5998d1f1c0dd67d1907
Nasty Balandina <askabalandina5556@gmail.com>
2026-01-14 12:28:55 +0300

    homework 14_задание_9

---
+++ lesson_14/math_operations/main.py
@@ -0,0 +1,8 @@
+from lesson_14.math_operations import add
+from lesson_14.math_operations import subtract
+
+sum_result = add(5, 3)
+diff_result = subtract(10, 4)
+
+print("Сумма:", sum_result)
+print("Разность:", diff_result)
