458389bb6ab391dcc40456337a31b9fa82958cd0
Анастасия Баландина <askabalandina5556@gmail.com>
2026-01-14 12:59:56 +0300

    Delete lesson_14/package directory

--- lesson_14/package/main.py
+++
@@ -1,8 +0,0 @@
-from lesson_14.package.math_operations import add
-from lesson_14.package.math_operations import subtract
-
-sum_result = add(5, 3)
-diff_result = subtract(10, 4)
-
-print("Сумма:", sum_result)
-print("Разность:", diff_result)
